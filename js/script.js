2e94455e9f1b48387fa4e19fb0dd778d145a6e1f
gribanov <gribanov@protei.ru>
2021-08-24 19:36:56 +0300

    Some new changes

--- js/script.js
+++ js/script.js
@@ -1,9 +1,14 @@
 $(document).ready(function(){
-    $(".about-small-text").hide();
-    $(".about-small-header").show();
-
     $('.about-small-header').click(function(){
       var btnId = $(this).data('id');
-      $(".about-small-text[data-id=" + btnId + "]").slideToggle();
+      $('.about-small-text[data-id=' + btnId + ']').slideToggle();
+
+      var attribute = $('.about-small-header[data-id=' + btnId + ']').attr('class');
+      if (attribute.indexOf('rounded-bottom') >= 0) {
+        attribute = attribute.replace('rounded-bottom','');
+      } else {
+        attribute = attribute + ' rounded-bottom';
+      }
+      $('.about-small-header[data-id=' + btnId + ']').attr('class', attribute);
     });
 });
