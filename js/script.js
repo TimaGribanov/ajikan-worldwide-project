d2554df89a354873dbfc7115cf2f1d2f70ec5742
gribanov <gribanov@protei.ru>
2021-08-25 20:07:18 +0300

    Started new page, mobile layout

--- js/script.js
+++ js/script.js
@@ -1,3 +1,29 @@
+//Check if mobile
+let detectMobile = () => {
+  const toMatch = [
+      /Android/i,
+      /webOS/i,
+      /iPhone/i,
+      /iPad/i,
+      /iPod/i,
+      /BlackBerry/i,
+      /Windows Phone/i
+  ];
+
+  return toMatch.some((toMatchItem) => {
+      return navigator.userAgent.match(toMatchItem);
+  });
+}
+
+//Adapt to mobile
+if(detectMobile() === true) {
+  $('head').append('<link rel="stylesheet" href="css/style_mobile.css">');
+}
+else {
+  $('head').append('<link rel="stylesheet" href="css/style.css">');
+}
+
+//Open and close boxes in About us
 $(document).ready(function(){
     $('.about-small-header').click(function(){
       var btnId = $(this).data('id');
